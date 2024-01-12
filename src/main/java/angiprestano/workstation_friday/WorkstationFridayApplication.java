0e042a9239aae4b19750745f60bb95d338d5d56b
Angela Prestano <angiprestano99@gmail.com>
2024-01-12 11:51:16 +0100

    Creation and first class

---
+++ src/main/java/angiprestano/workstation_friday/WorkstationFridayApplication.java
@@ -0,0 +1,13 @@
+package angiprestano.workstation_friday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkstationFridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkstationFridayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angiprestano/workstation_friday/WorkstationFridayApplicationTests.java
@@ -0,0 +1,13 @@
+package angiprestano.workstation_friday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkstationFridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
